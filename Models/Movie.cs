d862764710170547330d0892f8d148ed9c81663c
Dauren <Dauren_tawer228@mail.ru>
2021-02-20 03:05:11 +0500

    RazerPage Movie Project

---
+++ Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+        public string Poster { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
